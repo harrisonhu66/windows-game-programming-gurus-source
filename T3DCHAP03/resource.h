466875aa1b7b1202656e4e00101d1374f0dd1a46
harrisonhu66 <harrisonhu66@gmail.com>
2026-05-09 18:50:14 +0800

    Temporarily stop

---
+++ T3DCHAP03/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by demo3_1.rc
+//
+#define ICON_T3DX                       100
+#define JUMP                            101
+#define CURSOR_CROSSHAIR                200
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
